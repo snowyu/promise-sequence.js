9a3bd125edd15f8b77c5a5c70875ca63b0c05ed6
Riceball LEE <snowyu.lee@gmail.com>
2015-07-31 10:47:46 +0800

    - remove catch error, you should catch error via yourself

--- src/any.js
+++ src/any.js
@@ -1,20 +1,21 @@
 var Promise   = require('any-promise');
+
 var isArray   = Array.isArray;
 
 /*
- @param aList {Array} pass it as argument to the task.
+ @param aList {Array} each item as argument pass to the task.
  @param task {Promise} promise of task function
 */
+
 module.exports = function any(aList, task){
   function _genReduceFn(fn) {
     return function (previous, item){
-      if (previous == null)
-        previous = fn(item).catch(function(){});
+      if (previous == null) {
+        previous = fn ? fn(item) : item;
+      }
       return previous;
     };
   }
-
-  if (!isArray(aList)) aList = [aList];
 
   return Promise.reduce(aList, _genReduceFn(task), null);
 };
--- src/some.js
+++ src/some.js
@@ -2,20 +2,28 @@ var Promise   = require('any-promise');
 var isArray   = Array.isArray;
 
 /*
- @param aList {Array} pass it as argument to the task.
- @param task {Promise} promise of task function
+  @param aList {Array} each item as argument pass to the task.
+  @param total {Number} optional
+  @param task {Promise} promise of task function
 */
+function _undefined(i){
+  return (i !== void 0);
+}
 module.exports = function some(aList, total, task){
   function _genReduceFn(fn) {
     return function (previous, item){
-      previous = previous.filter(Boolean);
-      if (previous.length < total)
-        previous = Promise.all(previous.concat(fn(item).catch(function(){})));
+      previous = previous.filter(_undefined);
+      if (!total || previous.length < total) {
+        previous = Promise.all(previous.concat(fn ? fn(item):item)).filter(_undefined);
+      }
       return previous;
     };
   }
 
-  if (!isArray(aList)) aList = [aList];
+  if (typeof total !== 'number') {
+    task        = total;
+    total       = null;
+  }
 
   return Promise.reduce(aList, _genReduceFn(task), []);
 };
