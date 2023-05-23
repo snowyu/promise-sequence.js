eabf6265f0213c459d885800b92c93f01692b294
Riceball LEE <snowyu.lee@gmail.com>
2023-05-23 20:46:29 +0800

    fix(pipe): pass this to task feat(pipe): stop pipeline if throw EPipeStop error

--- src/pipeline.js
+++ src/pipeline.js
@@ -1,40 +1,48 @@
-// var Promise   = require('any-promise');
-var isArray   = Array.isArray;
-var cast      = Promise.cast || Promise.resolve;
+const isArray   = Array.isArray;
+const cast      = Promise.cast || Promise.resolve;
 
-/*
+export class EPipeStop extends Error {}
+
+/**
   Run a set of task functions in sequence, passing the result
   of the previous as an argument to the next.  Like a shell
   pipeline, e.g. `cat file.txt | grep 'foo' | sed -e 's/foo/bar/g'
 
-  @param tasks {Array|Promise} array or promise for array of task functions
-  @param initialArgs {Array} arguments to be passed to the first task
-  @param self {*} `this` argument to be passed to the first task
+  @param {Array|Promise} tasks array or promise for array of task functions
+  @param {Array} [initialArgs]  arguments to be passed to the first task
+  @param {*} [self]  `this` argument to be passed to the first task
   @return {Promise} promise for return value of the final task
 */
-function pipeline(tasks, aArgs, self){
+export function pipeline(tasks, aArgs, self){
   if (!isArray(aArgs)) {
     self = aArgs;
     aArgs = [];
   }
   if (tasks && !isArray(tasks)) tasks = [tasks];
   if (!self) self = this;
-  return Promise.all(aArgs).then(function(args){
+  return Promise.all(aArgs).then(async function(args){
     if (tasks && tasks.length) {
-      var current = cast.call(Promise);
-      current = current.then(function(){
-        var task = tasks[0];
-        return task && task.apply ? task.apply(self, args) : task;
-      });
-      tasks.slice(1).forEach(function(task){
-        current = current.then(typeof task === 'function' ? task : function(){return task});
-      });
-      return current;
+      let result
+      for (const task of tasks) {
+        if (task && task.apply) {
+          try {
+            result = await task.apply(self, args);
+            args = [result]
+          } catch (err) {
+            if (!(err instanceof EPipeStop)) {throw err}
+            break;
+          }
+        } else {
+          args = [task]
+          result = task
+        }
+      }
+
+      return result;
     } else {
       return args;
     }
   });
 };
 
-module.exports = pipeline
-module.exports.default = pipeline
+export default pipeline
